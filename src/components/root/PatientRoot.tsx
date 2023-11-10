4d4d3c472839bc8d6542b01c56f227bbae004a3b
Reydel Ocon <reydel321@gmail.com>
2023-11-10 17:39:33 +0800

    send me your location

--- src/components/root/PatientRoot.tsx
+++ src/components/root/PatientRoot.tsx
@@ -11,9 +11,58 @@ import {
 import { IoMdNotificationsOutline } from 'react-icons/io'
 
 import Notification from '../Notification'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import moment from 'moment'
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+
+type PatientDataTypes = {
+  patient_id: number
+  created_at: string
+  patient_age: number
+  patient_birthday: string
+  patient_email: string
+  patient_gender: string
+  patient_name: string
+  patient_lastname: string
+  patient_middlename: string
+  patient_phone: string
+  patient_type: string
+}
 
 export default function HProviderRoot() {
   const location = useLocation()
+
+  const patient_id = localStorage.getItem('patient_id')
+  const user_id = localStorage.getItem('user_id')
+
+  const [patientData, setPatientData] = useState<PatientDataTypes[]>([])
+
+  const getPatientData = () => {
+    axios
+      .get('http://localhost/prenatal-tb/patient.php', {
+        params: {
+          patient_id: patient_id,
+          // user_id: user_id,
+        },
+      })
+      .then((res) => {
+        setPatientData(res.data)
+        // console.log(res.data)
+      })
+  }
+
+  useEffect(() => {
+    getPatientData()
+  }, [])
 
   return (
     <div className="flex w-full flex-col justify-center items-center h-screen">
@@ -32,8 +81,77 @@ export default function HProviderRoot() {
           </Popover>
         </header>
         <div className="w-full border-2 px-2">
-          {location.pathname === '/' ? <App /> : <Outlet />}
+          <div className="flex justify-between items-center">
+            {patientData.map((patient, index) => (
+              <div className="flex flex-col p-4" key={index}>
+                <span className="flex  gap-2 text-3xl">
+                  Hello,
+                  <h1 className="font-bold">
+                    {' '}
+                    {patient.patient_name +
+                      ' ' +
+                      patient.patient_middlename +
+                      ' ' +
+                      patient.patient_lastname}
+                  </h1>
+                  👋
+                </span>
+
+                <p>
+                  See your number of visits and get notified when your
+                  appointment is set
+                </p>
+              </div>
+            ))}
+
+            <span className="font-semibold text-lg">
+              {moment().format('LLLL')}
+            </span>
+          </div>
         </div>
+
+        <div className="flex gap-2 p-2">
+          <Card className="text-start bg-white">
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">yes</CardTitle>d
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">10</div>
+              <p className="text-xs text-muted-foreground">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Provident, accusantium?
+              </p>
+            </CardContent>
+          </Card>
+
+          <Card className="text-start bg-white">
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">yes</CardTitle>d
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">10</div>
+              <p className="text-xs text-muted-foreground">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Provident, accusantium?
+              </p>
+            </CardContent>
+          </Card>
+
+          <Card className="text-start bg-white">
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">yes</CardTitle>d
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">10</div>
+              <p className="text-xs text-muted-foreground">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Provident, accusantium?
+              </p>
+            </CardContent>
+          </Card>
+        </div>
+
+        <div className="p-2">your next appointment is on date date</div>
       </div>
     </div>
   )
